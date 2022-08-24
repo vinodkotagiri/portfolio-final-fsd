735f748f59ea9cc187967b413698cc8ce912d61b
vinodkotagiri <vinodkotagiri@icloud.com>
2022-08-24 13:08:16 +0530

    initial skills added;

--- src/components/Skillspage/component.skills.jsx
+++ src/components/Skillspage/component.skills.jsx
@@ -1,7 +1,72 @@
 import React from 'react';
-
+import ParticlesBg from 'particles-bg';
+import { Stack, Typography, Box } from '@mui/material';
+import { MySkills } from './skills';
+import { Lightbulb } from '@mui/icons-material';
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div>
+      <ParticlesBg num={5} type='square' bg={true} />
+      <Stack
+        sx={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: '32px',
+        }}
+      >
+        <Stack
+          direction='row'
+          gap={2}
+          sx={{ justifyContent: 'center', alignItems: 'center' }}
+        >
+          <Lightbulb sx={{ fontSize: '32px' }} color='secondary' />
+          <Typography variant='h4' color='secondary'>
+            Skills
+          </Typography>
+        </Stack>
+      </Stack>
+      <Box
+        sx={{
+          backgroundColor: '#00000095',
+          width: { md: '75%', xs: '100%' },
+          height: { md: '65%', xs: '0' },
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%,-40%)',
+          padding: '2rem',
+          borderRadius: '4rem',
+
+          boxShadow: '2px 2px 32px 8px #9124A3',
+        }}
+      >
+        <Stack
+          direction={{ xs: 'column', md: 'row' }}
+          gap={6}
+          sx={{
+            flexWrap: 'wrap',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          {MySkills.map((skill) => (
+            <Stack
+              sx={{
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}
+              key={skill.id}
+            >
+              <img src={skill.logo} alt='html-logo' style={{ width: '5rem' }} />
+              <Typography color='#FFFfff98' variant='p'>
+                {skill.name}
+              </Typography>
+            </Stack>
+          ))}
+        </Stack>
+      </Box>
+    </div>
+  );
 };
 
 export default Skills;
---
+++ src/components/Skillspage/skills.js
@@ -0,0 +1,130 @@
+import {
+    HtmlLogo,
+    CssLogo,
+    JSLogo,
+    TSLogo,
+    FigmaLogo,
+    BSLogo,
+    TWLogo,
+    AntLogo,
+    MUILogo,
+    ReactLogo,
+    ReduxLogo,
+    NodeLogo,
+    PythonLogo,
+    MySqlLogo,
+    MongoDBLogo,
+    // RestLogo,
+    GraphQlLogo,
+    GitLogo,
+    AWSLogo,
+    GCPLogo
+} from '../../assets'
+
+export const MySkills = [
+    {
+        id: 1,
+        name: 'HTML',
+        logo: HtmlLogo,
+    },
+    {
+        id: 2,
+        name: 'CSS',
+        logo: CssLogo,
+    },
+    {
+        id: 3,
+        name: 'Javascript',
+        logo: JSLogo,
+    },
+    {
+        id: 4,
+        name: 'TypeScript',
+        logo: TSLogo,
+    },
+    {
+        id: 5,
+        name: 'Figma',
+        logo: FigmaLogo,
+    },
+    {
+        id: 6,
+        name: 'BootStrap',
+        logo: BSLogo,
+    },
+    {
+        id: 7,
+        name: 'Tailwind',
+        logo: TWLogo,
+    },
+    {
+        id: 8,
+        name: 'Ant Design',
+        logo: AntLogo,
+    },
+    {
+        id: 9,
+        name: 'MaterialUI',
+        logo: MUILogo,
+    },
+    {
+        id: 10,
+        name: 'HTML',
+        logo: HtmlLogo,
+    },
+    {
+        id: 11,
+        name: 'React',
+        logo: ReactLogo,
+    },
+    {
+        id: 21,
+        name: 'Redux',
+        logo: ReduxLogo,
+    },
+    {
+        id: 12,
+        name: 'NodeJS',
+        logo: NodeLogo,
+    },
+    {
+        id: 13,
+        name: 'Python',
+        logo: PythonLogo,
+    },
+    {
+        id: 14,
+        name: 'MySQL',
+        logo: MySqlLogo,
+    },
+    {
+        id: 15,
+        name: 'MongoDB',
+        logo: MongoDBLogo,
+    },
+    {
+        id: 16,
+        name: 'GraphQL',
+        logo: GraphQlLogo,
+    },
+    {
+        id: 17,
+        name: 'GIT',
+        logo: GitLogo,
+    },
+    {
+        id: 18,
+        name: 'AWS',
+        logo: AWSLogo,
+    },
+    {
+        id: 19,
+        name: 'GCP',
+        logo: GCPLogo,
+    },
+    {
+        id: 20,
+        name: 'HTML',
+        logo: HtmlLogo,
+    },
+]
