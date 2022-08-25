27fc7884707b5943c61be507e81c568f9fb4be17
vinodkotagiri <vinodkotagiri@icloud.com>
2022-08-25 15:45:15 +0530

    completed portfolio page

--- src/components/Portfoliopage/component.portfolio.jsx
+++ src/components/Portfoliopage/component.portfolio.jsx
@@ -1,42 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   Stack,
   Typography,
+  Button,
   Card,
+  Grid,
   CardMedia,
   CardContent,
-  Button,
   CardActions,
-  Grid,
-  Modal,
-  Box,
 } from '@mui/material';
 import ParticlesBg from 'particles-bg';
-import { Assessment } from '@mui/icons-material';
+import { Assessment, Preview, Code } from '@mui/icons-material';
 import { Projects } from './projects';
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '75%',
-  bgcolor: 'secondary.light',
-  border: '1px solid #000',
-  boxShadow: 24,
-  borderRadius: '32px',
-  p: 4,
-};
-
 const Portfolio = () => {
-  const [open, setOpen] = React.useState(false);
-  const [modalId, setModalId] = useState(0);
-  const handleOpen = (event) => {
-    setOpen(true);
-    console.log(event.target.value);
-    setModalId(event.target.value - 1);
-  };
-  const handleClose = () => setOpen(false);
   return (
     <div>
       <ParticlesBg num={5} type='thick' bg={true} />
@@ -45,7 +22,7 @@ const Portfolio = () => {
           justifyContent: 'center',
           alignItems: 'center',
           marginTop: '32px',
-          marginBottom: '64px',
+          marginBottom: '8px',
         }}
       >
         <Stack
@@ -59,74 +36,70 @@ const Portfolio = () => {
           </Typography>
         </Stack>
       </Stack>
-      <Grid container spacing={2} sx={{ padding: '8px' }}>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          justifyContent: { xs: 'flex-start', md: 'center' },
+          alignItems: 'center',
+          marginLeft: '32px',
+        }}
+      >
         {Projects.map((project) => (
           <Grid item key={project.id}>
-            <Card sx={{ maxWidth: 345 }}>
+            <Card
+              sx={{
+                maxWidth: { xs: '80%', md: 400 },
+                borderRadius: 5,
+                height: 480,
+                width: { xs: '100%' },
+              }}
+            >
               <CardMedia
                 component='img'
-                height='140'
                 image={project.cover}
-                alt={`${project.name}-cover`}
+                alt={`${project.name}-logo`}
+                height={140}
               />
-              <CardContent>
-                <Typography gutterBottom variant='h4' component='div'>
-                  {project.name}
-                </Typography>
-                <Typography variant='body2' color='text.secondary'>
-                  {project.description}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button
-                  variant='outlined'
-                  color='secondary'
-                  value={project.id}
-                  size='small'
-                  onClick={handleOpen}
-                >
-                  Learn More
-                </Button>
-              </CardActions>
+              <Stack gap={4}>
+                <CardContent sx={{ height: 250 }}>
+                  <Typography variant='h4' color='#000'>
+                    {project.name}
+                  </Typography>
+                  <Typography variant='p' color='#616161'>
+                    {project.description}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    endIcon={<Preview />}
+                    variant='contained'
+                    size='large'
+                  >
+                    Demo
+                  </Button>
+                  <Button
+                    startIcon={<Code />}
+                    variant='outlined'
+                    color='secondary'
+                    size='large'
+                  >
+                    Frontend
+                  </Button>
+                  <Button
+                    startIcon={<Code />}
+                    variant='outlined'
+                    color='secondary'
+                    size='large'
+                  >
+                    Backend
+                  </Button>
+                </CardActions>
+              </Stack>
             </Card>
           </Grid>
         ))}
       </Grid>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby='modal-modal-title'
-        aria-describedby='modal-modal-description'
-      >
-        <Box sx={style}>
-          <Typography id='modal-modal-title' variant='h3' component='h2'>
-            {Projects[modalId].name}
-          </Typography>
-          <Typography id='modal-modal-description' sx={{ mt: 2 }}>
-            {Projects[modalId].techs}
-          </Typography>
-          <Stack direction='row' gap={2} sx={{ marginTop: '2rem' }}>
-            <Button
-              variant='contained'
-              onClick={() => window.open(`${Projects[modalId].deployedUrl}`)}
-            >
-              View Demo
-            </Button>
-            <Button
-              variant='outlined'
-              onClick={() => window.open(`${Projects[modalId].frontEndCode}`)}
-            >
-              Frontend Code
-            </Button>
-            <Button
-              variant='outlined'
-              onClick={() => window.open(`${Projects[modalId].backEndCode}`)}
-            >
-              Backend Code
-            </Button>
-          </Stack>
-        </Box>
-      </Modal>
     </div>
   );
 };
--- src/components/Portfoliopage/projects.js
+++ src/components/Portfoliopage/projects.js
@@ -3,24 +3,16 @@ export const Projects = [
     {
         id: '1',
         name: 'Membook',
-        description: 'Membook is a facebook clone web application built with React as Front end and NodeJS as scalable backend. Membook features a strong authentication system and advanced form validation techniques and more..',
+        description: 'Welcome to Membook, the best social media app for staying connected with your friends and family. Membook is the perfect way to keep in touch with loved ones all over the world. With Membook, you can share photos, videos, and thoughts with your friends and family whenever and wherever you want. Membook is the ultimate social media app for staying connected with the people you care about most.',
         cover: require('../../assets/images/social-media.png'),
         frontEndCode: '',
         backEndCode: '',
-        deployedUrl: '',
-        techs: `Used React and MaterialUi for front end design.
-        Used Express and NodeJS for backend develpment. 
-        Form validation is handled by Formik and Yup. 
-        MongoDB is used for database. 
-        Authentication is based on JSON Web tokens with verification system. 
-        Statemanagement is handled by Redux. 
-        Created protected routes for specific secured pages. 
-        Added a dark mode  with ability to toggle between. The app is fully responsive and created using different reusable components`,
+        deployedUrl: ''
     },
     {
         id: '2',
         name: 'SCart',
-        description: '',
+        description: 'SCart is a new and revolutionary e-commerce platform that allows businesses to sell their products and services online in a more efficient and cost-effective manner. SCart offers a wide range of features and benefits that are not available with other e-commerce platforms, including a built-in payment processing system, advanced order management system, and a powerful search engine.',
         cover: require('../../assets/images/ecommerce-cover.png'),
         frontEndCode: '',
         backEndCode: '',
@@ -29,7 +21,7 @@ export const Projects = [
     {
         id: '3',
         name: 'NuChat',
-        description: '',
+        description: 'Introducing NuChat! This is the ultimate communication app that lets you chat and have group video calls with your friends and family. With NuChat, you can keep in touch with loved ones no matter where they are in the world. Plus, with our cutting-edge video call technology, you can see and hear them in stunning HD quality. Try NuChat today and experience the best way to stay connected!',
         cover: require('../../assets/images/nuchat-hero.jpg'),
         frontEndCode: '',
         backEndCode: '',
@@ -38,7 +30,7 @@ export const Projects = [
     {
         id: '4',
         name: 'Blocks',
-        description: '',
+        description: "Blocks is a content management system that enables businesses to manage their website's content without having to rely on expensive and time- consuming web development services.Blocks makes it easy for businesses to create, edit, and publish website content without having any html or css knowledge.With Blocks, businesses can easily keep their website's content up-to-date and SEO optimized.",
         cover: require('../../assets/images/cms.jpg'),
         frontEndCode: '',
         backEndCode: '',
